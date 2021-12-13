a60be536c8811cc3e550ab0581efd6e6aaa798c6
torn6810 <torn6810@gmail.com>
2021-12-12 17:12:59 -0800

    Added Dictionary component and search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-       hello from 
+       Dictionary App
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">
+        <a href="https://github.com/torn6810/dictionary-project"> Open Source Code{" "}</a> 
+      by Edith Tornel</footer>
+      </div>
     </div>
   );
 }

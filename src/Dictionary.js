5d4d14d0f6ec05d391536d5d027575c316a89fb7
torn6810 <torn6810@gmail.com>
2021-12-15 11:29:52 -0800

    Created meaning and results components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,25 @@
 import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary(){
     const[keyword, setKeyword]=useState("");
+    const[results, setResults]=useState(null);
+
+    function searchWord(response){
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
+    }
 
     function search(event){
         event.preventDefault();
         alert (`Searching for "${keyword}"`);
-    }
 
-    function searchWord(response){
-        console.log(response);
+        //Documentation: https://dictionaryapi.dev/
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        axios.get(apiUrl).then(searchWord);
     }
-
-    let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
-    axios.get(apiUrl).then(searchWord);
 
     function handleWordSubmit(event){
         console.log(event.target.value);
@@ -27,7 +31,8 @@ export default function Dictionary(){
             <form onSubmit={search}>
                 <input type="search" placeholder="Enter a word" autoFocus={true} onChange={handleWordSubmit}/>
                 <input type="submit" value="Search"/>
-            </form>    
+            </form>
+            <Results results={results}/>    
         </div>
     )
 }

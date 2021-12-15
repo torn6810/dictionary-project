b36e3bea251fba5e11072a626bef1797682df4d0
torn6810 <torn6810@gmail.com>
2021-12-15 11:32:15 -0800

    Removed alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,8 +14,7 @@ export default function Dictionary(){
 
     function search(event){
         event.preventDefault();
-        alert (`Searching for "${keyword}"`);
-
+       // alert (`Searching for "${keyword}"`);
         //Documentation: https://dictionaryapi.dev/
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
         axios.get(apiUrl).then(searchWord);

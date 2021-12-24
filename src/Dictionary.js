9665212ccf772d1ccebd3218d18adc6f342e5961
torn6810 <torn6810@gmail.com>
2021-12-23 21:17:26 -0800

    Added Photos component and photo styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,33 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props){
     const[keyword, setKeyword]=useState(props.defaultKeyword);
     const[results, setResults]=useState(null);
     const[loaded, setLoaded]=useState(false);
+    const[photos, setPhotos]=useState(null);
 
-    function searchWord(response){
-        console.log(response.data[0].meanings[0].definitions[0].definition);
+    function searchDictionaryResponse(response){
+        //console.log(response.data[0].meanings[0].definitions[0].definition);
         setResults(response.data[0]);
+    }
+
+    function searchPexelsResponse(response){
+        console.log(response);
+        setPhotos(response.data.photos);
     }
 
     function search(){
         //Documentation: https://dictionaryapi.dev/
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
-        axios.get(apiUrl).then(searchWord);
+        axios.get(apiUrl).then(searchDictionaryResponse);
+
+        let pexelsApiKey="563492ad6f917000010000019e22b0c7059546448b4b80b31738ef25";
+        let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers={ Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, {headers:headers}).then(searchPexelsResponse);
     }
 
     function handleSubmit(event){
@@ -44,7 +56,8 @@ export default function Dictionary(props){
                 <input type="submit" value="Search" className="Search-button"/>
             </form>
             </section>
-            <Results results={results}/>    
+            <Results results={results}/>
+            <Photos photos={photos}/>    
         </div>
     );
 } else{
--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,7 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props){
     return(
         <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
+            <h3 className="part-of-speech">{props.meaning.partOfSpeech}</h3>
             {props.meaning.definitions.map(function(definition, index)
             {
                 return(
